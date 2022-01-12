a7cedbab3b0e7786dba6d6f77cfadd453dc48964
nine9249 <576422858@qq.com>
2022-01-12 19:29:18 +0800

    1. 完成用户搜索查询

--- src/main/java/com/yu_JJ/bean/Result.java
+++ src/main/java/com/yu_JJ/bean/Result.java
@@ -8,14 +8,17 @@ import java.util.List;
 public class Result<T>{
     private Integer retCode;//0表示失败，，1表示成功
     private String retMsg;
+    private Integer count;
     private T retObj;
 
-    public Result() {
+    public Result(Integer retCode) {
+        this.retCode = retCode;
     }
 
-    public Result(Integer retCode, String retMsg, T retObj) {
+    public Result(Integer retCode, String retMsg, Integer count, T retObj) {
         this.retCode = retCode;
         this.retMsg = retMsg;
+        this.count = count;
         this.retObj = retObj;
     }
 }
